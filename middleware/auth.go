14be6124581703b71fcfed2b8a59db889753eb79
iswust6136 <9387693+iswust6136@user.noreply.gitee.com>
2023-02-03 16:04:32 +0800

    更正auth中间件的bug

--- middleware/auth.go
+++ middleware/auth.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"github.com/RaymondCode/simple-demo/service"
+	"github.com/RaymondCode/simple-demo/utils"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"strings"
@@ -19,21 +20,29 @@ func Auth() gin.HandlerFunc {
 			}
 		}
 		token := c.Query("token")
-		if len(token) == 0 {
-			c.JSON(http.StatusOK, service.Response{
-				StatusCode: 1,
-				StatusMsg:  "认证失败！",
-			})
+		id := c.Query("id")
+		_, claim, err := utils.ParseToken(token)
+		// 检测token是否过期
+		if err != nil {
+			error("认证失败！token已过期", c)
+			c.Abort()
 			return
 		}
-		cache, ok := service.ConcurrentMap.Load(token)
-		if !ok || cache == nil {
-			c.JSON(http.StatusOK, service.Response{
-				StatusCode: 1,
-				StatusMsg:  "认证失败！",
-			})
-			return
+		// 检测是否携带id，如果携带与claim里面的id比较
+		if len(id) != 0 {
+			if claim.Id != id {
+				error("认证失败！你没有权限执行此操作", c)
+				c.Abort()
+				return
+			}
 		}
 		c.Next()
 	}
+}
+
+func error(msg string, c *gin.Context) {
+	c.JSON(http.StatusOK, service.Response{
+		StatusCode: 1,
+		StatusMsg:  msg,
+	})
 }
